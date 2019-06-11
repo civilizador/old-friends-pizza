622a989be5de31822d9953d2b2ec96de0644d3f7
Asim Salimli <asimsalimli@Asims-MacBook-Pro.local>
2019-06-10 22:26:22 -0400

    1. Cart added 2.App code refactored. 3. Items List moved out to separate component

--- client/src/components/main_comps/CartPage.js
+++ client/src/components/main_comps/CartPage.js
@@ -3,56 +3,82 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {removeFromCart} from '../../actions';
 
-
-
 class CartPage extends React.Component{
-   countItemsInCart(){
+  countItemsInCart(){
     if(this.props.store.auth)
     return this.props.store.auth.cart.length
   }
-  
   renderCartItems(){
     if(this.props.store.auth){
         return(
             this.props.store.auth.cart.map((cartItem, index)=>{
                 return (
-                    <li key={index} className="list-group-item">
-                        <div>
-                            <img src={cartItem.image} className='cartItemImage' alt="" />
-                            {cartItem.name}
-                            <span className='float-right'><i onClick={()=>{this.removeCartItem(index)}} className="far fa-times-circle"></i></span>
-                        </div>
-                    </li>)
+                  <li  key={index} className="list-group-item">
+                    <div className='row'>
+                      <div class="col-sm-2 col-xs-2 col-md-2">
+                        <img src={cartItem.image} className='cartItemImage' alt="" />
+                      </div>
+                      <div class="col-xs-3 col-sm-3  col-md-3">
+                         <p className='CartPageText'> {cartItem.name} </p>
+                      </div>
+                      <div class="col-xs-3 col-sm-3  col-md-3">
+                          <p className='CartPageText'> Extra </p>
+                      </div>
+                      <div class="col-xs-2 col-sm-2 col-md-2 ">
+                        <p className='CartPageText'> Total </p>
+                      </div>
+                      <div class="col-xs-2 col-sm-2 col-md-2 ">
+                      <span><i onClick={()=>{this.removeCartItem(index)}} className="far fa-times-circle"></i></span>
+                      </div>
+
+                      </div>
+                  </li>
+                )
             })
-            
         )
     }else{
        return(
-          <div className='mx-auto'> 
+          <div className='mx-auto'>
             <hr />
             <p>Your Cart is Empty</p>
-        </div>    
-        ) 
+        </div>
+        )
     }
   }
-  
   removeCartItem(itemIndex){
       console.log('Cart.js line 26', itemIndex)
     this.props.removeFromCart(itemIndex)
   }
-  
+
   render(){
       return(
-             
                 <div className="container" style={{marginTop: "15rem"}} aria-labelledby="navbarDropdown">
                     <ul className="list-group">
+                    <li    className="list-group-item">
+                      <div className='row'>
+                        <div class="col-sm-2 col-xs-2 col-md-2">
+                         </div>
+                        <div class="col-xs-3 col-sm-3  col-md-3">
+                           <p className='CartPageText'> Name </p>
+                        </div>
+                        <div class="col-xs-3 col-sm-3  col-md-3">
+                            <p className='CartPageText'> Extra </p>
+                        </div>
+                        <div class="col-xs-2 col-sm-2 col-md-2 ">
+                          <p className='CartPageText'> Total </p>
+                        </div>
+                        <div class="col-xs-2 col-sm-2 col-md-2 ">
+                          Remove
+                          </div>
+
+                        </div>
+                    </li>
                         {this.renderCartItems()}
                     </ul><br/>
                  </div>
-           
           )
   }
- 
+
 }
 const mapStateToProps = (store)=>({store})
 export default connect(mapStateToProps,{removeFromCart})(CartPage)
